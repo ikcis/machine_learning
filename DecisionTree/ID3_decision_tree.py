f69c1341f0923475e2f0250ced76c44df7fe7e56
Ye Kaikai <ykkczh@gmail.com>
2019-02-06 20:46:15 +0800

    Update

--- DecisionTree/ID3_decision_tree.py
+++ DecisionTree/ID3_decision_tree.py
@@ -12,17 +12,33 @@ class ID3_Decision_Tree():
     def predict(self, test_data, test_feature_name):
         pass
 
-    def vote(self, categories):
-        return 0
+    def cal_shannon_entropy(self, data_set):
 
-    def generate_tree(self, data_set, feature_name):
+    def choose_best_feature(self, data_set):
+        feature_len = len(data_set[0])-1
+        data_len = len(data_set)
+        data_set_entropy =
+
+    def vote(self, categories):
+        cate_count = {}
+        for cate in categories:
+            if cate in cate_count.keys():
+                cate_count[cate] += 1
+            else:
+                cate_count[cate] = 1
+        voted_category = max(cate_count.items(), key=lambda x: x[1])[0]
+        return voted_category
+
+    def generate_tree(self, data_set, features_name):
         categories = [example[-1] for example in data_set]
         if categories.count(categories[0]) == len(categories):
             return categories[0]
         if len(data_set[0]) == 1:
             return self.vote(categories)
 
-        _, best_feature_id = self.
+        _, best_feature_id = self.choose_best_feature(data_set)
+
+        best_feature_name = features_name[best_feature_id]
 
 
 id3_tree_object = ID3_Decision_Tree()
