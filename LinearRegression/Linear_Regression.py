bd0e98ea4bc2f666b939bd0cb590cf135c0ef91c
Ye Kaikai <ykkczh@gmail.com>
2019-02-09 23:49:21 +0800

    Update

--- LinearRegression/Linear_Regression.py
+++ LinearRegression/Linear_Regression.py
@@ -9,16 +9,17 @@ class Linear_Regression:
         self.n_iters = None
 
     def sgd(self, X, Y, m):
-        cost = np.sum(np.dot((np.dot(X, self.W) - Y).T, (np.dot(X, self.W) - Y)) / 2 * m)
+        # cost = np.dot((np.dot(X, self.W) - Y).T, (np.dot(X, self.W) - Y)) / 2 * m
+        cost = np.sum(np.square(np.dot(X, self.W) - Y)) / 2 * m
         self.W = self.W - self.learning_rate * np.dot(X.T, (np.dot(X, self.W) - Y)) / m
         return cost
 
-    def fit(self, X, Y, learning_rate=0.01, n_iters=1000):
+    def fit(self, X, Y, learning_rate=0.01, n_iters=800):
         self.learning_rate = learning_rate
         self.n_iters = n_iters
         m = X.shape[0]
         n = X.shape[1]
-        self.W = np.random.normal(size=(X.shape[1], 1))
+        self.W = np.random.normal(size=(X.shape[1],))
         cost_list = []
         for i in range(n_iters):
             cost = self.sgd(X, Y, m)
@@ -33,7 +34,7 @@ class Linear_Regression:
 m = 80  # 样本数
 n = 7  # 特征数
 X_train = np.random.normal(size=(m, n))
-W_train_true = np.random.normal(size=(X_train.shape[1], 1))
+W_train_true = np.random.normal(size=(X_train.shape[1],))
 Y_no_noise = np.dot(X_train, W_train_true)
 Y_train = Y_no_noise + np.random.normal(scale=0.25, size=Y_no_noise.shape)
 model = Linear_Regression()
--- LinearRegression/test.py
+++ LinearRegression/test.py
@@ -1,11 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-m = 80  # 样本数
-n = 7  # 特征数
-X_train = np.random.normal(size=(m, n))
-W_train_true = np.random.normal(size=(X_train.shape[1], 1))
-Y_no_noise = np.dot(X_train, W_train_true)
-Y_train = Y_no_noise + np.random.normal(scale=0.25, size=Y_no_noise.shape)
-print(type(X_train))
-X_train.T * (np.dot(X_train, W_train_true) - Y_train)
+a = np.array([1, 2, 3])
+b = np.array([[1],[2],[3]])
+print(np.sum(b,axis=1))
